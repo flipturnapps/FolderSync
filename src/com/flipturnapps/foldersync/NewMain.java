629268ab65e9140f4bb7f6e33342f9185dc22019
flipturnapps <flipturnapps@gmail.com>
2015-06-06 06:31:06 +0800

    Fixed Mistake in NewMain

--- src/com/flipturnapps/foldersync/NewMain.java
+++ src/com/flipturnapps/foldersync/NewMain.java
@@ -40,7 +40,7 @@ public class NewMain
 	}
 	public static File getStartDir() 
 	{
-		String out = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath();
+		String out = new File(NewMain.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath();
 		File outfile = new File(out);
 		File parFile = new File(outfile.getParent().replace("%20", " "));
 		return parFile;
